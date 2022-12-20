9be879bcd7c8d60427cf8533ec2ecd60615feece
James MacRe <jmacre18@bw.edu>
2022-12-19 22:03:32 -0500

    delta test changes

--- android/src/com/mygdx/NEBULA/AndroidLauncher.java
+++ android/src/com/mygdx/NEBULA/AndroidLauncher.java
@@ -25,5 +25,4 @@ public class AndroidLauncher extends AndroidApplication {
 	public AndroidAudio createAudio(Context context, AndroidApplicationConfiguration config) {
 		return new AsynchronousAndroidAudio(context, config);
 	}
-
 }
