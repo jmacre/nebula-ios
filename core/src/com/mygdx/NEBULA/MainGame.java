839f5d9cc76d7e709bb401c17d9d6ddf38078494
James MacRe <jmacre18@bw.edu>
2022-12-21 12:19:39 -0500

    delta changes

--- core/src/com/mygdx/NEBULA/MainGame.java
+++ core/src/com/mygdx/NEBULA/MainGame.java
@@ -365,7 +365,9 @@ public class MainGame extends GameElements implements Screen {
         if (isPaused || isRunningResumeCountdown) {
             deltaP = 0;
         }
-         for (int i = 0; i < deltaList.size; i++) {
+
+        if (deltaList.size >= 60) {
+            for (int i = 0; i < deltaList.size; i++) {
                 deltaSum += deltaList.get(i);
             }
             delta = deltaSum / deltaList.size;
@@ -375,7 +377,10 @@ public class MainGame extends GameElements implements Screen {
             } else {
                 deltaP = delta;
             }
+            deltaList.removeIndex(0);
             deltaSum = 0;
+        }
+
 
         if (fadeOutOpacity > 0 || fadeInOpacity > 0)
             blackTransition.draw(game.batch);
@@ -1920,13 +1925,15 @@ public class MainGame extends GameElements implements Screen {
         if (!isTransitioningOut && !isFadingOut) {
             isPaused = true;
         }
+
+//        songPausePosition = mainMusic.getCursorPosition();
     }
 
     @Override
     public void resume() {
         if (Gdx.graphics.getDisplayMode().refreshRate != refreshRate) {
             refreshRate = Gdx.graphics.getDisplayMode().refreshRate;
-                deltaList.clear();
+            deltaList.clear();
         }
     }
 
