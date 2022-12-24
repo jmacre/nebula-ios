ec3ed5c29ecdbe5c9f0a2fe00f26b0cf0972d6fc
James MacRe <jmacre18@bw.edu>
2022-12-24 11:52:11 -0500

    inputprocessor movement

--- core/src/com/mygdx/NEBULA/MainGame.java
+++ core/src/com/mygdx/NEBULA/MainGame.java
@@ -209,7 +209,7 @@ public class MainGame extends GameElements implements Screen {
 
     ShapeRenderer sr = new ShapeRenderer();
 
-    MyInputProcessor inputProcessor = new MyInputProcessor();
+    MyInputProcessor inputProcessor = new MyInputProcessor(this);
 
     Array<Bullet> bullets = new Array<>();
     Array<Bullet> bulletsToRemove = new Array<>();
@@ -242,6 +242,7 @@ public class MainGame extends GameElements implements Screen {
     float refreshRate;
 
     Bullet bullet1, bullet2, bullet3, bullet4;
+    public MainGame(){};
 
     public MainGame(Main game, GameInterface gameInterface, Assets assets, Background background) {
         this.game = game;
@@ -859,23 +860,7 @@ public class MainGame extends GameElements implements Screen {
     }
 
     public void movePlayer() {
-        if (!isPaused && isTransitionedIn && !isShipLeaving && !isFadingOut && !isTransitioningOut) {
-            if (!(Gdx.input.getY() < (int) (SCREEN_HEIGHT / 5f)) && SHIP_START_Y > SHIP_Y) {
-                    shipMovementVal = (deltaP * moveSpeed * (Gdx.input.getX() - SHIP_X - SHIP_WIDTH / 2f));
-                    if (SHIP_X + shipMovementVal > SCREEN_WIDTH - SHIP_WIDTH) {
-                        SHIP_X = SCREEN_WIDTH - SHIP_WIDTH;
-                    }
 
-                    else if (SHIP_X + shipMovementVal < 0) {
-                        SHIP_X = 0;
-                    }
-                    else {
-                        SHIP_X += (int)shipMovementVal;
-                    }
-
-                CURRENT_SHIP_X = SHIP_X;
-            }
-        }
     }
 
     public void runResumeCountdown(float delta) {
--- core/src/com/mygdx/NEBULA/MyInputProcessor.java
+++ core/src/com/mygdx/NEBULA/MyInputProcessor.java
@@ -1,10 +1,18 @@
 package com.mygdx.NEBULA;
 
+import static com.mygdx.NEBULA.GameElements.SHIP_X;
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.input.GestureDetector;
 
 public class MyInputProcessor implements InputProcessor {
     private int tapReleaseCount = 0;
     private int tapCount = 0;
+    MainGame m;
+    public MyInputProcessor(MainGame m){
+        this.m = m;
+    };
     @Override
     public boolean keyDown(int keycode) {
         return false;
@@ -22,20 +30,44 @@ public class MyInputProcessor implements InputProcessor {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        movePlayer(screenX, screenY, pointer);
         tapCount++;
         return false;
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        movePlayer(screenX, screenY, pointer);
         tapReleaseCount++;
         return false;
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
+        movePlayer(screenX, screenY, pointer);
         return false;
     }
+
+    private void movePlayer(int screenX, int screenY, int pointer){
+        if(pointer == 0) {
+
+            if (!m.isPaused && m.isTransitionedIn && !m.isShipLeaving && !m.isFadingOut && !m.isTransitioningOut) {
+                if (!(screenY < (int) (GameElements.SCREEN_HEIGHT / 5f)) && m.SHIP_START_Y > GameElements.SHIP_Y) {
+                    m.shipMovementVal = (m.deltaP * m.moveSpeed * (screenX - SHIP_X - GameElements.SHIP_WIDTH / 2f));
+                    if (SHIP_X + m.shipMovementVal > GameElements.SCREEN_WIDTH - GameElements.SHIP_WIDTH) {
+                        SHIP_X = GameElements.SCREEN_WIDTH - GameElements.SHIP_WIDTH;
+                    } else if (SHIP_X + m.shipMovementVal < 0) {
+                        SHIP_X = 0;
+                    } else {
+                        SHIP_X += (int) m.shipMovementVal;
+                    }
+
+                    GameElements.CURRENT_SHIP_X = SHIP_X;
+                }
+            }
+        }
+    }
+
 
     @Override
     public boolean mouseMoved(int screenX, int screenY) {
@@ -53,4 +85,5 @@ public class MyInputProcessor implements InputProcessor {
     public int getTapCount() {
         return tapCount;
     }
+
 }
